b5fedff0eed90baee124e65a45fecb226d2f448f
hansparsons <hansp@outlook.com>
2014-09-01 15:33:26 -0700

    h

--- rootforsite/myscripts.js
+++ rootforsite/myscripts.js
@@ -1,18 +1,3 @@
-var sliderOptions =
-{
-	sliderId: "slider",
-	startSlide: 0,
-	effect: "series1",
-	effectRandom: false,
-	pauseTime: 2800,
-	transitionTime: 90,
-	slices: 14,
-	boxes: 8,
-	hoverPause: 1,
-	autoAdvance: true,
-	captionOpacity: 0.4,
-	captionEffect: "fade",
-	thumbnailsWrapperId: "thumbs",
-	m: false,
-	license: "mylicense"
-};
+$('#myDiv').on('hide.bs.collapse', function (e) {
+  preventDefault(e);
+})
